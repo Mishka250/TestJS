af3ffca81b4dfb19e95ee4ecb9fbb108c777428c
Mishka250 <sidorenko.m.r@gmail.com>
2016-04-22 13:53:06 +0300

    Initial commit

---
+++ firstTry.js
@@ -0,0 +1,11 @@
+/**
+ * Created by admin on 19.04.2016.
+ */
+function method(x1,x2){return x1+x2}
+function method2(x1,s2) {
+    var ca = x1*s2
+    if((x1>0)&&(x1<0))
+        return ca*ca
+    else
+        alert('12312')
+}
