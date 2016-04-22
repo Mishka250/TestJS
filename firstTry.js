357ae4587b31ed0161dbc5dbf1e40d6dc224d990
Mishka250 <sidorenko.m.r@gmail.com>
2016-04-22 14:02:23 +0300

    multiplied for 2

--- firstTry.js
+++ firstTry.js
@@ -5,7 +5,7 @@ function method(x1,x2){return x1+x2}
 function method2(x1,s2) {
     var ca = x1*s2
     if((x1>0)&&(x1<0))
-        return ca*ca
+        return ca*ca*2
     else
         alert('12312')
 }
